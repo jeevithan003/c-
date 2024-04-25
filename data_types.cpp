50752a3ca4ca29406f5f6cade972dee647427f11
Jeevithan <117728163+jeevithan003@users.noreply.github.com>
2024-04-26 02:01:34 +0530

    Create data_types.cpp

---
+++ data_types.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    // data types in cpp
+    int goals=2;
+     double avg_goalcontibution=12.24;
+     float physs=2.4;
+     bool isvar=true;
+     string name="haaland";
+     cout<<name<<endl;
+     cout<<physs<<endl;
+     cout<<isvar<<endl;
+     cout<<avg_goalcontibution<<endl;
+}
