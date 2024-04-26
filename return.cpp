e38cc0112393fb62c72e0fcebb0a404fea011528
Jeevithan <117728163+jeevithan003@users.noreply.github.com>
2024-04-26 16:56:10 +0530

    Create return.cpp

---
+++ return.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<math.h>
+#include<time.h>
+#include<string>
+using namespace std;
+string myfun(){
+  return "hello bro";
+
+}
+int main(){
+
+
+
+cout<<myfun()<<endl;
+  return 0;
+}
