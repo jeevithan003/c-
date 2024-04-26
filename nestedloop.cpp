25734d19610cccd4b53d8cbb5d2ad40051c8f9f3
Jeevithan <117728163+jeevithan003@users.noreply.github.com>
2024-04-26 16:27:23 +0530

    Create nestedloop.cpp

---
+++ nestedloop.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+int main(){
+
+for (int i = 0; i < 10; i++)
+{
+  for (int j = 0; j < 11; j++)
+  {
+   /* code */
+   cout<<"#";
+  }
+  cout<<" "<<endl;
+  
+}
+
+
+
+
+  return 0;
+}
