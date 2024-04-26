e101f9aa002fd24ffe9f6a09beb4eeb56ba08334
Jeevithan <117728163+jeevithan003@users.noreply.github.com>
2024-04-26 16:22:01 +0530

    Create math.cpp

---
+++ math.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+int main(){
+
+ //math opertaions is cpp
+ int a=2;
+ int b=5;
+ int z;
+ cout<<max(a,b)<<endl;
+ cout<<pow(a,b)<<endl;
+ cout<<sqrt(b)<<endl;
+ cout<<log10(b)<<endl;
+ cout<<round(4.32)<<endl;
+cout<<cbrt(a)<<endl;
+cout<<exp(b)<<endl;
+
+
+
+
+  return 0;
+}
