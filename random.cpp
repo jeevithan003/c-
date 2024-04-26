9b1b79a54375c5db01ad28754e30639ce5deee2e
Jeevithan <117728163+jeevithan003@users.noreply.github.com>
2024-04-26 16:49:56 +0530

    Create random.cpp

---
+++ random.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<math.h>
+#include<time.h>
+using namespace std;
+int main(){
+
+srand(time(NULL));
+int a= (rand()%6+1);
+int b=(rand()%6+1);
+cout<<b<<endl;
+cout<<a<<endl;
+  return 0;
+}
