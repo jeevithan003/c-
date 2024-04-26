26d5eaa33d0b1eb31fbe9aeacc4fb022d9ed674d
Jeevithan <117728163+jeevithan003@users.noreply.github.com>
2024-04-26 16:23:44 +0530

    Create ifelse.cpp

---
+++ ifelse.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+int main(){
+ int a=101;
+ if(a==10){
+   cout<<"woo"<<endl;
+
+ }
+ else{
+   cout<<"owww"<<endl;
+ }
+
+
+
+
+
+  return 0;
+}
