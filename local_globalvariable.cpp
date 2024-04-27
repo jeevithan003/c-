f240d7bf02e08f5701ea53e0eb45257e14cfede4
Jeevithan <117728163+jeevithan003@users.noreply.github.com>
2024-04-27 22:11:24 +0530

    Create local_globalvariable.cpp

---
+++ local_globalvariable.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<math.h>
+#include<time.h>
+#include<string>
+using namespace std;
+string player="neymar jr";
+int main(){
+//local varriable and global variable in cpp
+string player="neymary de silava";
+cout<<player<<endl;//local
+cout<<::player<<endl;//gloabal
+
+return 0;
+
+
+}
